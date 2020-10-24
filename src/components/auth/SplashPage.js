c577dd983902f484b3fc792701e54bd47ef51ae7
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-10-23 22:41:31 -0500

    Added a splash page and a client login

--- src/components/auth/SplashPage.js
+++ src/components/auth/SplashPage.js
@@ -1,1 +1,26 @@
 //Page that asks visitors whether they are a coach or a client
+import React from "react";
+import { Header, Image } from "semantic-ui-react"
+import { Link } from "react-router-dom"
+import "./SplashPage.css"
+import mainLogo from "./images/happy_analytics_200x200.png";
+
+export const SplashPage = () => {
+
+    return (
+    <main>
+        <Header as="h1">
+            <Image src={mainLogo} className="headLogo" />
+            Welcome To Happy Analytics
+        </Header>
+        <Header as="h3">Are you a Client or a Coach?</Header>
+        <div className="choiceOfLogin">
+            <section className="link--login">
+                    <Link to="/client.login">I am a Client</Link>
+            </section>
+            <section className="link--login">
+                    <Link to="/coach.login">I am a Coach</Link>
+            </section>
+        </div>
+    </main>)
+}
